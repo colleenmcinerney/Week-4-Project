cfc8b8a68e81133b0abfedcfc87190856b624196
colleenmcinerney <colleenmcinerney@Colleeens-MacBook-Air.local>
2016-12-20 09:07:14 -0600

    week 5 work

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function goToLocation(href) {
     location.href = href;
 }
+
+document.getElementById("intro").innerHTML = "Planning Your Vacation"
+
+if (x < 500){
+    greeting = "less than $500";
+} else {
+    greeting = "more than $500";
+}
